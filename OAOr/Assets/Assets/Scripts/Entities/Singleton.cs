a3304f22c95affaf77cd1f89ce557e2f6a742069
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2025-01-16 22:31:15 +0900

    chore: 라이브러리 추가

---
+++ OAOr/Assets/Assets/Scripts/Entities/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.DesignPattern
+{
+    public abstract class Singleton<T> where T : Singleton<T>, new()
+    {
+        private static T _instance;
+
+        public static T Instance { get { return _instance; } }
+
+        public Singleton()
+        {
+            if (_instance == null)
+            {
+                _instance = (T)this;
+            }
+        }
+    }
+}
