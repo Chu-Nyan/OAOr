fa9287d4ab8ee3e69a9dd8a18894e461794e00c3
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2025-01-21 01:55:41 +0900

    feat: MonoBehaviour 싱글톤 추가

---
+++ OAOr/Assets/Assets/Scripts/Entities/MonoBehaviourSingleton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Library.DesignPattern
+{
+    public abstract class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviourSingleton<T>, new()
+    {
+        private static T _instance;
+
+        public static T Instance 
+        { 
+            get 
+            {
+                if (_instance == null)
+                {
+                    var name = typeof(T).ToString();
+                    _instance = new GameObject(name).AddComponent<T>();
+                }
+
+                return _instance; 
+            } 
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = (T)this;
+            }
+        }
+    }
+}
